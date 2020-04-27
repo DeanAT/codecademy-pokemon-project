27741a57feb328abf13134a962a96673d32191e1
DeanAT <64385178+DeanAT@users.noreply.github.com>
2020-04-27 16:28:29 +1200

    Add files via upload

--- pokemon.py
+++ pokemon.py
@@ -1,21 +1,24 @@
-#Codecademy Pokemon project for pythonclass Pokemon:
-  def __init__(self, name, level, type1, max_health, current_health, base_attack, dead=False):
+#Codecademy Pokemon Project
+class Pokemon:
+  def __init__(self, name, level, type1, max_health, current_health, base_attack,exp, dead=False):
     self.name = name.title()
     self.level = level
     self.type1 = type1
+    self.exp = exp
     self.dead = dead
     self.max_health = max_health
     self.current_health = current_health
     self.base_attack = base_attack
-    return print(" Name: {p1} \n Level: {l1} \n Type: {t1} \n Max Health: {m1} \n Current Health: {h1} \n".format(p1=self.name, l1=self.level, t1=self.type1, m1=self.max_health, h1=self.current_health))
+    
+    return print(" Name: {p1} \n Level: {l1} \n Type: {t1} \n Max Health: {m1} \n Current Health: {h1} \nExprience:  {e3}\n".format(p1=self.name, l1=self.level, t1=self.type1, m1=self.max_health, h1=self.current_health, e3=self.exp))
     
   def lose_health(self,hit):
     dead = None
     self.current_health = self.current_health - hit
     if dead == True:
-      print(self.name, " is dead")
+      print(self.name, " is ko'd")
     if self.current_health < 1:
-      print("{p1} has died. RIP".format(p1=self.name))
+      print("{p1} has been ko'd".format(p1=self.name))
     else:
       return print("{p1} has {h1} health left\n".format(h1=self.current_health, p1=self.name)), self.current_health, dead
   
@@ -31,16 +34,26 @@
     dead = False
     rival.dead = dead
     if self.current_health < 1:
-      return print("{} is dead and cannot attack anyone".format(self.name))
+      return print("{} is ko'd and cannot attack anyone".format(self.name))
     if rival.current_health < 1:
       rival.dead = True
-      return print(rival.name, " is dead you cannot attack.\n"), rival.dead
+      return print(rival.name, " is ko'd you cannot attack.\n"), rival.dead
     if self.type1 == "fire" or self.type1 == "ice" and rival.type1 == "grass":
       rival.current_health = rival.current_health - self.base_attack * 1.5
     else:# self.type1 == "grass" or self.type1 == "ice" and rival.type1 == "fire":
       rival.current_health = rival.current_health - self.base_attack
     if rival.current_health < 1:
-        print("\n" + rival.name + " has benn ko'd after being attacked by {}. Sad!.".format(self.name)),
+        print("\n" + rival.name + " has benn ko'd after being attacked by {}. Sad!.".format(self.name))
+        self.exp = self.exp + 50
+        return self.exp
+    if self.exp > 100:
+      self.exp = self.exp - 100
+      self.level = self.level + 1
+      self.base_attack = self.base_attack * 2
+      self.current_health = self.current_health * 2
+      self.max_halth = self.max_health * 2
+      print("{} has levelled up! now at level {}!!\n\n".format(self.name, self.level))
+      print(" Name: {p1} \n Level: {l1} \n Type: {t1} \n Max Health: {m1} \n Current Health: {h1} \nExprience:  {e3}\n".format(p1=self.name, l1=self.level, t1=self.type1, m1=self.max_health, h1=self.current_health, e3=self.exp))
     else:  
       print("{s2} has been attacked! by {self} with {t1}! {s2} has {h1} health left.".format(s2=rival.name,t1=self.type1, self=self.name, h1=rival.current_health))
       
@@ -98,19 +111,19 @@ class Trainer:
         self.active_pokemon = i
         print("{} is now active pokemon".format(self.active_pokemon.name))
         
-pika = Pokemon("pikachu", 4, "ice", 99, 45, 10)
-meow = Pokemon("meowf", 4, "grass", 99, 34, 13)
-jigg = Pokemon("jigglypuff", 4, "fire", 99, 39, 11)
-spiff = Pokemon("spiffamungus", 2, "ice", 99, 69, 12)
+pika = Pokemon("pikachu", 4, "ice", 99, 45, 10, 61)
+meow = Pokemon("meowf", 4, "grass", 99, 34, 13, 44)
+jigg = Pokemon("jigglypuff", 4, "fire", 99, 39, 11, 23)
+spiff = Pokemon("spiffamungus", 2, "ice", 99, 69, 12, 9)
 
 pika.lose_health(45)
 pika.regain_health(4)
 pika.attack(jigg)
 jigg.attack(spiff)
 meow.attack(jigg)
-meow.attack(jigg)
-meow.attack(jigg)
-meow.attack(jigg)
+pika.attack(jigg)
+pika.attack(jigg)
+pika.attack(jigg)
 pika.attack(spiff)
 trainer1 = Trainer("brian",[pika, meow, jigg,spiff], 10 ,40, meow)
 trainer2 = Trainer("Jen", [spiff, jigg], 4 ,34, jigg)
